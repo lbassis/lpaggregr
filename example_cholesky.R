537b6c5a7f2d89643890777c8dc1af120df760af
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2023-11-17 01:15:37 -0300

    small changes to see all p's

--- example_cholesky.R
+++ example_cholesky.R
@@ -1,21 +1,30 @@
+library(tidyverse)
 library(devtools)
 install_github("dosimont/lpaggregr")
 library(lpaggregr)
 
+TRACE <- "cholesky_11520_960_starpu_25_3_dmda_1_idcin-2.grenoble.grid5000.fr_2016-08-21_20-49-12.pjdump"
+
 #Threshold: 0<th<1, lower value means more accuracy for retrieving the list of partitions but longer computation time
-th=0.001
+th=0.0001
 
 #Without hierarchy
-trace=parsepjdump("cholesky_11520_960_starpu_25_3_dmda_1_idcin-2.grenoble.grid5000.fr_2016-08-21_20-49-12.pjdump")
+trace=parsepjdump(TRACE)
 
 #Example of filtering
-trace$data<-trace$data[!(trace$data$Value %in% c('Idle','Sleeping')),]
+trace$data<-trace$data[(trace$data$Value %in% c('dgemm','dsyrk', 'dpotrf', 'dtrsm')),]
 
 #No hierarchy (because it's not present in the trace, but it should be...)
-micro=pjdump2micro(trace,50,"State",FALSE)
+micro=pjdump2micro(trace, 100,"State",FALSE)
 
 odf<-oaggregate(micro$data, th)
-
-odf
 qualplot(odf)
-oplot_stacked_state(omacro(odf$Partitions, micro, odf$POpt))
+#odf$POpt
+list_of_ps <- as.list(unique(odf$Partitions$Parameter, 3))
+str(list_of_ps)
+lapply(list_of_ps,
+       function(p){
+           oplot_stacked_state(omacro(odf$Partitions, micro, p)) +
+               coord_cartesian(ylim=c(0,0.0100), xlim=c(0,100))
+       }) -> z
+z
